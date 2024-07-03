881592798cf5571d974874e337f179e354bb97b8
chenshuai <chenshuai@ian2018.cn>
2024-07-03 22:23:32 +0800

    支持批量文件

--- uis/tab_gan_extract_line.py
+++ uis/tab_gan_extract_line.py
@@ -12,34 +12,39 @@ from app.tools import get_timestamp
 from app.tools import resolve_relative_path
 
 
-def upload_file(file):
+def upload_file(files):
     try:
-        root_path = resolve_relative_path(__file__, '../output')
+        results = []
+        for file in files:
+            root_path = resolve_relative_path(__file__, '../output')
 
-        output_dir = f'{root_path}/gen_line_outputs'
-        create_path(output_dir)
+            output_dir = f'{root_path}/gen_line_outputs'
+            create_path(output_dir)
 
-        source_dir = f'{root_path}/gen_line_inputs/{generate_random_id(12)}'
-        create_path(source_dir)
+            source_dir = f'{root_path}/gen_line_inputs/{generate_random_id(12)}'
+            create_path(source_dir)
 
-        file_name_split = os.path.split(file)[-1].split('.')
-        name_ext = file_name_split[-1]
-        new_path = f'{source_dir}/{get_timestamp()}_{generate_random_id(4)}.{name_ext}'
-        shutil.move(file, new_path)
+            file_name_split = os.path.split(file)[-1].split('.')
+            name_ext = file_name_split[-1]
+            new_path = f'{source_dir}/{get_timestamp()}_{generate_random_id(4)}.{name_ext}'
+            shutil.move(file, new_path)
 
-        result = extract_line_by_gan(f'{source_dir}/', f'{output_dir}/')[0]
-        svg_result = convert2svg_image(result, after_delete=True)
-        print(svg_result)
-        return gr.UploadButton(label="更换文件"), gr.Image(value=svg_result, type="filepath", visible=True)
+            result = extract_line_by_gan(f'{source_dir}/', f'{output_dir}/')[0]
+            svg_result = convert2svg_image(result, after_delete=True)
+            results.append(svg_result)
+        return gr.UploadButton(label="更换文件"), results
     except Exception as e:
         raise gr.Error(f"{e}, 请重试")
 
 
 def build_gan_extract_line_ui():
     with gr.TabItem("GAN 模型提取线稿", id=4):
-        image = gr.Image(format="svg", visible=False, width=512, show_label=False)
+        svg_gallery = gr.Gallery(
+            label="最终 svg 图", format="svg",
+            columns=4, rows=1, object_fit="contain")
         upload_button = gr.UploadButton(
             label="上传图片",
-            file_types=['image']
+            file_types=['image'],
+            file_count='multiple'
         )
-        upload_button.upload(upload_file, [upload_button], [upload_button, image])
+        upload_button.upload(upload_file, [upload_button], [upload_button, svg_gallery])
